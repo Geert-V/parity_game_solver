007fdd7be4dfbe0c765f33ed4ddbd790780252b0
Geert Verstegen <gverstegen@hotmail.com>
2017-03-19 20:26:03 +0100

    Added a parity game file as example.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod data;
 mod parser;
 
 fn main() {
-    let pg = String::from("parity 4;\n1 3 0 1,3,4 \"Europe\";\n0 6 1 4,2;\n4 5 1 0 \"Antarctica\";\n1 8 1 2,4,3 \"America\";\n3 6 0 4,2 \"Australia\";\n2 7 0 3,1,0,4 \"Asia\";");
-    let game = parser::parse(pg);
+    let pg = "pg_test.txt";
+    let game = parser::parse_from_file(pg.to_string());
 
-    println!("Game: {:?}", game);
+    println!("{:?}", game);
 }
